28ffe46fc4fad2b9a186417c8a76fdb5e501f5ca
Jakob Ehn <jakob@ehn.nu>
2023-02-05 16:11:22 +0000

    adding sql injection code for demo

--- orderprocessor/Controllers/OrderController.cs
+++ orderprocessor/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Dapr;
 using Dapr.Client;
+using System.Data.SqlClient;
 
 namespace orderprocessor.Controllers;
 
@@ -19,12 +20,17 @@ public class OrderCreatedController : ControllerBase
     [HttpPost("ordercreated")]
     public async Task Post([FromBody]Order order, [FromServices] DaprClient daprClient)
     {
-        //
-        //
-        //
         Console.WriteLine($"{DateTime.Now.ToLongTimeString()} Processing order " + order.Id);
         await daprClient.SaveStateAsync<Order>("statestore", order.Id.ToString(), order);        
         await Task.Delay(2000);
         Console.WriteLine($"{DateTime.Now.ToLongTimeString()} Done with order " + order.Id);
     }    
+
+    [HttpGet("badcontroller")]
+    public async Task BadControllerAction(string name, string id)
+    {
+        SqlConnection con = new SqlConnection("localdb");
+        var cmd = new SqlCommand("UPDATE dbo.Person SET Name = " + name + " WHERE Id = " + id, con);
+        var result = await cmd.ExecuteScalarAsync();
+    }        
 }
